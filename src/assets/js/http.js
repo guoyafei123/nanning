18596c9f97a5674536c2a15e1667b1cfcfc2e81c
guoyafei <guoyafei@51play.com>
2018-07-11 14:57:36 +0800

    new

--- src/assets/js/http.js
+++ src/assets/js/http.js
@@ -1,4 +1,25 @@
 import axios from 'axios';
+
+//http request 拦截器
+axios.interceptors.request.use(
+  config => {
+    // const token = getCookie('名称');注意使用的时候需要引入cookie方法，推荐js-cookie
+    config.data = JSON.stringify(config.data);
+    config.headers = {
+      'Content-Type':'application/x-www-form-urlencoded'
+    }
+    // if(token){
+    //   config.params = {'token':token}
+    // }
+    return config;
+  },
+  error => {
+    return Promise.reject(err);
+  }
+);
+
+
+
 /**
  * 封装post请求
  * @param url
