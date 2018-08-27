1f4f379af341df84239732a36f6e1433e8cfad24
liujian <1533284421@qq.com>
2018-08-27 12:04:44 +0800

    绘制热力图

--- src/components/publick/map.vue
+++ src/components/publick/map.vue
@@ -903,52 +903,43 @@
 					}
 				})
 
-				
+        let myChart = this.$echarts.init(document.getElementById("allmap"));
 				this.$fetch("/img/toolsvg/hangzhou-tracks.json")
 				.then(data => {
-					alert(1);
 					var points = [].concat.apply([], data.map(function (track) {
 						return track.map(function (seg) {
 							return seg.coord.concat([1]);
 						});
 					}));
-
-					let myChart = this.$echarts.init(document.getElementById("allmap"));
-					var option = {
-						animation: false,
-						bmap: {
-							center: [120.13066322374, 30.240018034923],
-							zoom: 14,
-							roam: true
-						},
-						visualMap: {
-							show: false,
-							top: 'top',
-							min: 0,
-							max: 5,
-							seriesIndex: 0,
-							calculable: true,
-							inRange: {
-								color: ['blue', 'blue', 'green', 'yellow', 'red']
-							}
-						},
-						series: [{
-							type: 'heatmap',
-							coordinateSystem: 'bmap',
-							data: points,
-							pointSize: 9,
-							blurSize: 6
-						}]
-					}
-
-					myChart.setOption(option);
-
-					if (!app.inNode) {
-						// 添加百度地图插件
-						var bmap = myChart.getModel().getComponent('bmap').getBMap();
-						bmap.addControl(new BMap.MapTypeControl());
-					}
-
+          let option;
+          myChart.setOption(option = {
+            animation: true,
+            bmap: {
+              center: [120.13066322374, 30.240018034923],
+              zoom: 14,
+              roam: true
+            },
+            visualMap: {
+              show: true,
+              top: 'top',
+              min: 0,
+              max: 5,
+              seriesIndex: 0,
+              calculable: true,
+              inRange: {
+                color: ['blue', 'blue', 'green', 'yellow', 'red']
+              }
+            },
+            series: [{
+              type: 'heatmap',
+              coordinateSystem: 'bmap',
+              data: points,
+              pointSize: 5,
+              blurSize: 6
+            }]
+          });
+          var bmap = myChart.getModel().getComponent('bmap').getBMap();
+          bmap.addControl(new BMap.MapTypeControl());
 				})
 			},
 			path_danger(){
