5145c6065f36d86282204fc97bec968b89eeea0d
王晓波 <k3laohan@163.com>
2018-08-29 18:44:53 +0800

    火情分析

--- src/components/publick/header.vue
+++ src/components/publick/header.vue
@@ -230,29 +230,98 @@
             </div>
           </div>
         <section class="col-sm-offset-3 col-sm-6">
-        <div class="row my-scroll">            
+        <div class="row my-scroll size-12">            
             <div class="col-sm-12">
                 <h2 id="section-1">火情地图</h2>
                 <!-- 地图 -->
-                <div class="fire-map">
+                <div class="fire-map margin-bottom10">
                   <div><!-- 地图容器 --></div>
                   <article><!-- 说明 --></article>
                 </div>
-                <hr>
                 <h2 id="section-2">事故概况</h2>
-                <p>Nullam hendrerit justo non leo aliquet imperdiet. Etiam in sagittis lectus. Suspendisse ultrices placerat accumsan. Mauris quis dapibus orci. In dapibus velit blandit pharetra tincidunt. Quisque non sapien nec lacus condimentum facilisis ut iaculis enim. Sed viverra interdum bibendum. Donec ac sollicitudin dolor. Sed fringilla vitae lacus at rutrum. Phasellus congue vestibulum ligula sed consequat.</p>
-                <p>Vestibulum consectetur scelerisque lacus, ac fermentum lorem convallis sed. Nam odio tortor, dictum quis malesuada at, pellentesque vitae orci. Vivamus elementum, felis eu auctor lobortis, diam velit egestas lacus, quis fermentum metus ante quis urna. Sed at facilisis libero. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Vestibulum bibendum blandit dolor. Nunc orci dolor, molestie nec nibh in, hendrerit tincidunt ante. Vivamus sem augue, hendrerit non sapien in, mollis ornare augue.</p>
-                <hr>
-                <h2 id="section-3">起火单位最近数据</h2>
-                <p>Integer pulvinar leo id risus pellentesque vestibulum. Sed diam libero, sodales eget sapien vel, porttitor bibendum enim. Donec sed nibh vitae lorem porttitor blandit in nec ante. Pellentesque vitae metus ipsum. Phasellus sed nunc ac sem malesuada condimentum. Etiam in aliquam lectus. Nam vel sapien diam. Donec pharetra id arcu eget blandit. Proin imperdiet mattis augue in porttitor. Quisque tempus enim id lobortis feugiat. Suspendisse tincidunt risus quis dolor fringilla blandit. Ut sed sapien at purus lacinia porttitor. Nullam iaculis, felis a pretium ornare, dolor nisl semper tortor, vel sagittis lacus est consequat eros. Sed id pretium nisl. Curabitur dolor nisl, laoreet vitae aliquam id, tincidunt sit amet mauris.</p>
-                <p>Phasellus vitae suscipit justo. Mauris pharetra feugiat ante id lacinia. Etiam faucibus mauris id tempor egestas. Duis luctus turpis at accumsan tincidunt. Phasellus risus risus, volutpat vel tellus ac, tincidunt fringilla massa. Etiam hendrerit dolor eget ante rutrum adipiscing. Cras interdum ipsum mattis, tempus mauris vel, semper ipsum. Duis sed dolor ut enim lobortis pellentesque ultricies ac ligula. Pellentesque convallis elit nisi, id vulputate ipsum ullamcorper ut. Cras ac pulvinar purus, ac viverra est. Suspendisse potenti. Integer pellentesque neque et elementum tempus. Curabitur bibendum in ligula ut rhoncus.</p>
-                <p>Quisque pharetra velit id velit iaculis pretium. Nullam a justo sed ligula porta semper eu quis enim. Pellentesque pellentesque, metus at facilisis hendrerit, lectus velit facilisis leo, quis volutpat turpis arcu quis enim. Nulla viverra lorem elementum interdum ultricies. Suspendisse accumsan quam nec ante mollis tempus. Morbi vel accumsan diam, eget convallis tellus. Suspendisse potenti.</p>
-                <hr>
-                <h2 id="section-4">历史数据分析</h2>
-                <p>Suspendisse a orci facilisis, dignissim tortor vitae, ultrices mi. Vestibulum a iaculis lacus. Phasellus vitae convallis ligula, nec volutpat tellus. Vivamus scelerisque mollis nisl, nec vehicula elit egestas a. Sed luctus metus id mi gravida, faucibus convallis neque pretium. Maecenas quis sapien ut leo fringilla tempor vitae sit amet leo. Donec imperdiet tempus placerat. Pellentesque pulvinar ultrices nunc sed ultrices. Morbi vel mi pretium, fermentum lacus et, viverra tellus. Phasellus sodales libero nec dui convallis, sit amet fermentum sapien auctor. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Sed eu elementum nibh, quis varius libero.</p>
-                <p>Vestibulum quis quam ut magna consequat faucibus. Pellentesque eget nisi a mi suscipit tincidunt. Ut tempus dictum risus. Pellentesque viverra sagittis quam at mattis. Suspendisse potenti. Aliquam sit amet gravida nibh, facilisis gravida odio. Phasellus auctor velit at lacus blandit, commodo iaculis justo viverra. Etiam vitae est arcu. Mauris vel congue dolor. Aliquam eget mi mi. Fusce quam tortor, commodo ac dui quis, bibendum viverra erat. Maecenas mattis lectus enim, quis tincidunt dui molestie euismod. Curabitur et diam tristique, accumsan nunc eu, hendrerit tellus.</p>
-                <p>Phasellus fermentum, neque sit amet sodales tempor, enim ante interdum eros, eget luctus ipsum eros ut ligula. Nunc ornare erat quis faucibus molestie. Proin malesuada consequat commodo. Mauris iaculis, eros ut dapibus luctus, massa enim elementum purus, sit amet tristique purus purus nec felis. Morbi vestibulum sapien eget porta pulvinar. Nam at quam diam. Proin rhoncus, felis elementum accumsan dictum, felis nisi vestibulum tellus, et ultrices risus felis in orci. Quisque vestibulum sem nisl, vel congue leo dictum nec. Cras eget est at velit sagittis ullamcorper vel et lectus. In hac habitasse platea dictumst. Etiam interdum iaculis velit, vel sollicitudin lorem feugiat sit amet. Etiam luctus, quam sed sodales aliquam, lorem libero hendrerit urna, faucibus rhoncus massa nibh at felis. Curabitur ac tempus nulla, ut semper erat. Vivamus porta ullamcorper sem, ornare egestas mauris facilisis id.</p>
-                <p>Ut ut risus nisl. Fusce porttitor eros at magna luctus, non congue nulla eleifend. Aenean porttitor feugiat dolor sit amet facilisis. Pellentesque venenatis magna et risus commodo, a commodo turpis gravida. Nam mollis massa dapibus urna aliquet, quis iaculis elit sodales. Sed eget ornare orci, eu malesuada justo. Nunc lacus augue, dictum quis dui id, lacinia congue quam. Nulla sem sem, aliquam nec dolor ac, tempus convallis nunc. Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla suscipit convallis iaculis. Quisque eget commodo ligula. Praesent leo dui, facilisis quis eleifend in, aliquet vitae nunc. Suspendisse fermentum odio ac massa ultricies pellentesque. Fusce eu suscipit massa.</p>
+                <div class="textandimg fire-survey">
+                  <article>
+                    <p>｛报警时间｝，｛位置｝｛报警源｝发起警报，报警时中控室值守人：{最近一次PC中控打卡人姓名}，警报时策略圈内的人员有｛人员名称、人员名称｝；确认人｝于｛时间｝将该次警报确认为火情，响应时长：{确认时间-报警时间}。；{关闭人}于{关闭时间}在系统将该火情关闭，累计持续时长：{关闭时间-报警时间}。<br>
+                    报警时天气{天气}，温度{温度}，湿度{湿度}，风力{风力}，策略圈内微型消防站{数量}个，最近消防站相距事故地点{距离}。
+                    </p>
+                  </article>
+                  <hr>
+                  <div class="row textandimg-main clearfix">              
+                    <div class="col-sm-4">
+                      <span>建筑风险评估 </span>
+                      <strong>安全评分 5.5 风险系数 55.2%</strong>
+                    </div>
+                    <div class="col-sm-8">
+                      <span>详细位置 </span>
+                      <strong>xxx</strong>
+                    </div>
+                    <div class="col-sm-4">
+                      <span>报警时间 </span>
+                      <strong>2018-02-02 12:52:32</strong>
+                    </div>                  
+                    <div class="col-sm-4">
+                      <span>确认时间 </span>
+                      <strong>2018-02-02 12:52:32</strong>
+                    </div>
+                    <div class="col-sm-4">
+                      <span>关闭时间 </span>
+                      <strong>2018-02-02 12:52:32</strong>
+                    </div>                  
+                    <div class="col-sm-4">
+                      <span>响应时长 </span>
+                      <strong class="font-blue">326秒</strong>
+                    </div>
+                    <div class="col-sm-8">
+                      <span>持续时长 </span>
+                      <strong class="font-blue">326秒</strong>
+                    </div>
+                    <div class="col-sm-4">
+                      <span>报警源 </span>
+                      <strong>xxx</strong>
+                    </div>
+                    <div class="col-sm-4">
+                      <span>确认人 </span>
+                      <strong>xxx</strong>
+                    </div>
+                    <div class="col-sm-4">
+                      <span>关闭人 </span>
+                      <strong>xxx</strong>
+                    </div>
+                    <div class="col-sm-12">
+                      <span>报警说明 </span>
+                      <strong>xxx</strong>
+                    </div>
+                    <div class="col-sm-12">
+                      <span>图片视频 </span>
+                      <ul class="fire-media list-inline">
+                        <li><img src="http://photocdn.sohu.com/20131130/Img391053859.jpg" alt="" height="80"></li>
+                        <li><img src="http://photocdn.sohu.com/20131130/Img391053859.jpg" alt="" height="80"></li>
+                      </ul>
+                    </div>
+                    <div class="col-sm-12">
+                      <span>确认说明 </span>
+                      <strong>xxx</strong>
+                    </div>
+                    <div class="col-sm-12">
+                      <span>图片视频 </span>
+                      <ul class="fire-media list-inline">
+                        <li><img src="http://photocdn.sohu.com/20131130/Img391053859.jpg" alt="" height="80"></li>
+                        <li><img src="http://photocdn.sohu.com/20131130/Img391053859.jpg" alt="" height="80"></li>
+                      </ul>
+                    </div>
+                    <div class="col-sm-12">
+                      <span>关闭说明 </span>
+                      <strong>xxx</strong>
+                    </div>
+                    <div class="col-sm-12">
+                      <span>图片视频 </span>
+                      <ul class="fire-media list-inline">
+                        <li><img src="http://photocdn.sohu.com/20131130/Img391053859.jpg" alt="" height="80"></li>
+                        <li><img src="http://photocdn.sohu.com/20131130/Img391053859.jpg" alt="" height="80"></li>
+                      </ul>
+                    </div>
+                  </div>
+                </div>                
             </div>
         </div>
         </section>
