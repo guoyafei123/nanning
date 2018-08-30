7b56be6a2ab113f5153d78d11440fbaa3222fbc0
guoyafei <guoyafei@51play.com>
2018-08-30 11:16:40 +0800

    人员名称修改

--- src/management/List_of_people/all.vue
+++ src/management/List_of_people/all.vue
@@ -232,7 +232,7 @@
             { required: true, trigger: 'blur', validator: validPhone }//这里需要用到全局变量
           ],
           nickName:[
-            { required: true, trigger: 'blur', validator: Name }
+            { required: true, trigger: 'blur',  message: '请填写姓名' }
           ],
           unitId:[
             { required: true, message: '请选择单位', trigger: 'change' }
